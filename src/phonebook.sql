b9fca024860cce3d56b4a634d80ec31c3a74ea3d
Tirtha Sharma <tirthagenze121@gmail.com>
2021-03-20 01:32:26 +0530

    Create phonebook.sql

---
+++ src/phonebook.sql
@@ -0,0 +1,10 @@
+create database Phonebook;
+use Phonebook;
+
+create table phonebook
+(id int primary key not null auto_increment,
+name varchar(50),
+phone_no varchar(10));
+
+
+select * from phonebook;
